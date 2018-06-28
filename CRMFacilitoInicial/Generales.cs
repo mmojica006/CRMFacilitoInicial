692a2e27344f61c5baf898e0efceaa708d5ab8fd
mmojica006 <musicmojica@gmail.com>
2018-06-28 13:16:30 -0600

    rest

---
+++ CRMFacilitoInicial/Generales.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMFacilitoInicial
+{
+    public class Generales
+    {
+        public static string emailEnvio = "musicmojica@hotmail.com";
+
+        public static string contraseniaEmail = "!Claro0102";
+    }
+}
